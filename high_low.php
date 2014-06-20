b58c839fe29b2a86c77ba90918681fdc004e05d3
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-06-20 16:15:05 -0500

    Updated code formatting.

--- high_low.php
+++ high_low.php
@@ -1,96 +1,40 @@
  <?php
 
 if (!is_numeric($argv[1]) || !is_numeric($argv[2])) {
-	echo "I have no idea what you're talking about, buddy.\n"; #jk you're not my buddy
-	exit(1);
+    echo "I have no idea what you're talking about, buddy.\n";
+    exit(1);
 }
 
-
+// prompt user guess a number
 fwrite(STDOUT, "Guess a number between {$argv[1]} and {$argv[2]}.");
 
+// create a number
 $number = mt_rand($argv[1], $argv[2]);
 
+// get a guess from the user
 $guess = (int) fgets(STDIN);
 
 $total = 1;
 
+// while the user hasn't guessed correctly
 while ($guess != $number) {
 
-	if ($guess < $number) { 
-		fwrite(STDOUT, "Higher\n");
-		$guess = (int) fgets(STDIN);
+    // if the guess is lower than the number then tell the user "higher"
+    // if the guess is higher than the number then tell the user "lower"
+    if ($guess < $number) { 
+        fwrite(STDOUT, "Higher\n");
+        $guess = (int) fgets(STDIN);
 
-	} 	elseif ($guess > $number) { 
-		fwrite(STDOUT, "Lower\n");
-		$guess = (int) fgets(STDIN);
+    } elseif ($guess > $number) { 
+        fwrite(STDOUT, "Lower\n");
+        $guess = (int) fgets(STDIN);
 
- 	} //	elseif ($guess == $number) { 
+    }
 
-	$total++;  
-
+    // add the guess count
+    $total++;
 }
 
-	fwrite(STDOUT, "Correct!\n");
-	fwrite(STDOUT, "It took you $total tries to guess this number.\n");
-
-
-// 	PHP_EOL;
-
-	//Nick's beautiful version
-
-// $number = mt_rand(1, 1000);
-// $total = 1;
-
-// fwrite(STDOUT, 'Guess the number!'); 
-// 	$guess = (int) fgets(STDIN);
-
-//  while($guess != $number){
-// 	$total++;
-
-// 	fwrite(STDOUT, ($guess < $number ? 'Higher' : 'Lower') . "!\n");
-// 	$guess = (int) fgets(STDIN);
-// }
-
-// fwrite(STDOUT, "Correct!\nIt took you {$total} tries to guess this number.\n");
-
-
-
-//notes
-
-// create a number
-
-// while the user hasn't guessed correctly
-
-	// prompt user guess a number
-
-	// get a guess from the user
-
-	// check the number and the guess
-
-	// if the guess is higher than the number then tell the user "lower"
-
-	// if the guess is lower than the number then tell the user "higher"
-
-	// add the guess count
-
-	// if guess and the number are the same say "winner!" and end the game
-
-// end the loop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// if guess and the number are the same say "winner!" and end the game
+fwrite(STDOUT, "Correct!\n");
+fwrite(STDOUT, "It took you $total tries to guess this number.\n");
